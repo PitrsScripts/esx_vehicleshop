acd8c165b67c751fcde53f78050afaa19869ea35
Pitrs <150464632+PitrsScripts@users.noreply.github.com>
2025-07-20 19:09:21 +0200

    Add files via upload

---
+++ esx_vehicleshop/sql/owned_vehicles.sql
@@ -0,0 +1,40 @@
+-- --------------------------------------------------------
+-- Hostitel:                     127.0.0.1
+-- Verze serveru:                10.4.32-MariaDB - mariadb.org binary distribution
+-- OS serveru:                   Win64
+-- HeidiSQL Verze:               12.8.0.6908
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+CREATE TABLE IF NOT EXISTS `owned_vehicles` (
+  `owner` varchar(46) DEFAULT NULL,
+  `plate` varchar(12) NOT NULL,
+  `vehicle` longtext DEFAULT NULL,
+  `type` varchar(20) NOT NULL DEFAULT 'car',
+  `job` varchar(20) DEFAULT NULL,
+  `stored` tinyint(4) NOT NULL DEFAULT 0,
+  `parking` varchar(60) DEFAULT NULL,
+  `pound` varchar(60) DEFAULT NULL,
+  `mileage` float DEFAULT 0,
+  `financed` tinyint(1) NOT NULL DEFAULT 0,
+  `glovebox` longtext DEFAULT NULL,
+  `trunk` longtext DEFAULT NULL,
+  `finance_data` longtext DEFAULT NULL,
+  `name` varchar(40) DEFAULT 'Unknown',
+  PRIMARY KEY (`plate`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
